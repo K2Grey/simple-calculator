a29927f48da6c97657cd817a4def3153dc917203
Grégory Troupenat <gregory.troupenat@gmail.com>
2020-09-25 10:47:09 +0200

    simple-calculator done :)

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("ok")
+function calculate () {
+    console.log('lancement du calcul...');
+    const firstInput = document.getElementById('firstValue');
+    const firstValue = parseInt(firstInput.value);
+    console.log(firstValue);
+    const secondValue = parseInt(document.getElementById('secondValue').value);
+    console.log(secondValue);
+    const resultElement = document.getElementById('result');
+
+    const operator = document.getElementById('operand').value;
+    let result = "";
+
+    if (!isNaN(firstValue) && !isNaN(secondValue)) {
+
+        if (operator === "+") {
+            result = firstValue + secondValue;
+        }
+        else if (operator === "-") {
+            result = firstValue - secondValue;
+        }
+        else if (operator === "*") {
+            result = firstValue * secondValue;
+        }
+        else if (operator === "/") {
+            result = firstValue / secondValue;
+        }
+        else {
+            alert("invalid operator");
+        }
+    
+    resultElement.innerHTML = result;
+
+    }
+    else {
+        alert("Les deux valeurs sont requises.")
+    }
+
+}
+
+const calculateButton = document.querySelector('#calculate-btn');
+calculateButton.addEventListener('click', calculate);
